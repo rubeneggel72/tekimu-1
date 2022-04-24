3a0d01f2c5da8db00939c5f28490627d5add4968
rubeneggel72 <rubeneggel@gmail.com>
2022-04-24 00:52:33 -0300

    make it better

---
+++ client/build/precache-manifest.55261c973bc192db88c59668a6ab4751.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "792c6d162588acf0b38b",
+    "url": "/static/css/main.19c4a374.chunk.css"
+  },
+  {
+    "revision": "792c6d162588acf0b38b",
+    "url": "/static/js/main.5c68e7fa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9e13dfbf935dd962b4cc",
+    "url": "/static/css/2.7c2bc37a.chunk.css"
+  },
+  {
+    "revision": "9e13dfbf935dd962b4cc",
+    "url": "/static/js/2.361dcf14.chunk.js"
+  },
+  {
+    "revision": "10b9c9abf2618b27dec2108c28e90d7f",
+    "url": "/static/media/default-profile-no-gender.10b9c9ab.png"
+  },
+  {
+    "revision": "4224c0ac11cec930adc97eadf3c1bcd5",
+    "url": "/static/media/home-background.4224c0ac.jpg"
+  },
+  {
+    "revision": "2d99d69bd5243a8ddbbed4059a6264c3",
+    "url": "/static/media/home-background-mobile.2d99d69b.jpg"
+  },
+  {
+    "revision": "e67e358c72e30a5ec593534154b91ea6",
+    "url": "/static/media/default-profile-man.e67e358c.jpg"
+  },
+  {
+    "revision": "201d17520f0f579146c4e7f45960dd39",
+    "url": "/static/media/default-profile-woman.201d1752.jpg"
+  },
+  {
+    "revision": "81116db5c927cd79e3515e06ef8aedeb",
+    "url": "/static/media/heart-loading.81116db5.gif"
+  },
+  {
+    "revision": "208f7a6930dd1501cecfa469fddf11ad",
+    "url": "/static/media/heart-broken.208f7a69.gif"
+  },
+  {
+    "revision": "91b1ee008fa842562785e98e71a2c77b",
+    "url": "/static/media/heart-anim.91b1ee00.gif"
+  },
+  {
+    "revision": "d4964b534d40aede11b76c85c9a4f7db",
+    "url": "/static/media/logo-brand.d4964b53.png"
+  },
+  {
+    "revision": "bcfdc4a378efecceea342fad686fe36e",
+    "url": "/static/media/match-love-anim.bcfdc4a3.webp"
+  },
+  {
+    "revision": "bcb73808e0dd013dea1ed1ff2c113a78",
+    "url": "/static/media/no-image.bcb73808.jpeg"
+  },
+  {
+    "revision": "8c7fc3e64dd41dbbcbabebe6cb2ba3aa",
+    "url": "/static/media/man-profile-background.8c7fc3e6.png"
+  },
+  {
+    "revision": "cfcad439a7f0f32a38568bf54b0f6261",
+    "url": "/static/media/woman-profile-background.cfcad439.png"
+  },
+  {
+    "revision": "82b4cd696aca1fdbdcae0da6639541b2",
+    "url": "/static/media/man-woman-profile-background.82b4cd69.png"
+  },
+  {
+    "revision": "08ac7f5e9e0750ff62f1a0d17260cb86",
+    "url": "/static/media/logo.08ac7f5e.png"
+  },
+  {
+    "revision": "05f3730cde3b786ec7513fef84f4b6f1",
+    "url": "/index.html"
+  }
+];
